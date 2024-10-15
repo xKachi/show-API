09e3b5061e2cccae465a2d84b260ee27568f95e8
xKachi <kindtheideator@gmail.com>
2024-10-15 14:49:05 +0100

    block-3

--- Block-3-Registering-a-POST-Route/models/show.go
+++ Block-3-Registering-a-POST-Route/models/show.go
@@ -4,9 +4,9 @@ import "time"
 
 type Show struct {
 	ID          int
-	Description string
-	Location    string
-	DateTime    time.Time
+	Description string `binding:"required"`
+	Location    string	`binding:"required"`
+	DateTime    time.Time `binding:"required"`
 	UserID int // user that created the show
 }
 
@@ -15,4 +15,8 @@ var shows []Show = []Show{}
 func (s Show) Save() {
 	// later: add to database
 	shows = append(shows, s)
+}
+
+func GetAllShows() []Show {
+	return shows
 }
