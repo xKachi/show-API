556208e65e1aca2b65c33c5da4d2dcd392e39494
xKachi <kindtheideator@gmail.com>
2024-10-18 13:17:54 +0100

    Block-10

---
+++ Block-10-Registering-an-UpdateEvent-route-and-Handler/routes/routes.go
@@ -0,0 +1,11 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func RequestRoutes(server *gin.Engine) {
+server.GET("/shows", getShows)
+server.POST("/shows", createShows)
+server.GET("/shows/:id", getShow)
+server.PUT("/shows/:id", updateShow)
+}
+
