0c2478339b792db3955259836eb9416c36dc583f
xKachi <kindtheideator@gmail.com>
2024-10-20 14:59:45 +0100

    block-12

---
+++ Block-12-Deleting-Events/routes/routes.go
@@ -0,0 +1,12 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func RequestRoutes(server *gin.Engine) {
+server.GET("/shows", getShows)
+server.POST("/shows", createShows)
+server.GET("/shows/:id", getShow)
+server.PUT("/shows/:id", updateShow)
+server.PUT("/shows/:id", deleteShow)
+}
+
